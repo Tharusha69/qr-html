3b2e6635e2840494132993045c45eb82ec5dd7bf
Aswin Sparky <113657908+A-S-W-I-N-S-P-A-R-K-Y@users.noreply.github.com>
2024-01-25 16:45:29 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+var http = require('http');
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname+"/kop/index.html");
+})
+app.listen(3000, () => {
+  console.log(`Example app listening...`);
+})
