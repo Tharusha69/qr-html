1182c01b759b7d7440997d9860be60168978e033
Aswin Sparky <113657908+A-S-W-I-N-S-P-A-R-K-Y@users.noreply.github.com>
2024-01-25 16:47:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 var http = require('http');
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname+"/Qr.html");
+  res.sendFile(__dirname+"/sperky/Qr.html");
 })
 app.listen(3000, () => {
   console.log(`Example app listening...`);
